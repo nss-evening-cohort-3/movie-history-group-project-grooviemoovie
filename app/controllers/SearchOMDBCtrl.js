11449c734c2d91ad4ee017b4f9585ac695b54b85
David Cramb <david.cramb@gmail.com>
2016-06-04 09:40:48 -0500

    API call working, only console logs results right now

--- app/controllers/SearchOMDBCtrl.js
+++ app/controllers/SearchOMDBCtrl.js
@@ -1,12 +1,16 @@
 "use strict";
 
 app.controller('SearchOMDBCtrl', function($scope, $location, $routeParams, temporaryFactory){
-  
+  $scope.searchResults = [];
   $scope.searchMovie = "";
 
   $scope.searchForMovie = function() {
-    // console.log('searching for:', $scope.searchMovie);
-    temporaryFactory.searchOMDB($scope.searchMovie);
+    console.log('searching for:', $scope.searchMovie);
+    temporaryFactory.searchOMDB($scope.searchMovie)
+      .then(function(searchResults){
+        $scope.searchResults = searchResults;
+        console.log($scope.searchResults)
+      })
   }
 
 
@@ -18,13 +22,15 @@ app.factory('temporaryFactory', function($q, $http){
   var searchOMDB = function(searchMovie) {
     var items = []
     return $q(function(resolve, reject){
-      $http.get(`http://www.omdbapi.com/?t=${searchMovie}&y=&plot=short&type=&r=json`)
+      $http.get(`http://www.omdbapi.com/?s=${searchMovie}&y=&plot=short&type=&r=json`)
         .success(function(movieObject){
-          console.log(movieObject)
+          // console.log(movieObject)
           var searchResults = movieObject;
-        }) //.success
+          // console.log(searchResults.Search);
+          items.push(searchResults.Search);
+          // console.log(items);
           resolve(items);
-          console.log(searchResults)
+        }) //.success
     }); //$q
 
   

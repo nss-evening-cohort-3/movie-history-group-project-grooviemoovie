4992c299ca4e5266be29398a025b257b538b0551
David Cramb <david.cramb@gmail.com>
2016-06-04 13:26:22 -0500

    refactored code, moved tempfactory code out of controller and into ItemFActory/Itemstorage

--- app/controllers/SearchOMDBCtrl.js
+++ app/controllers/SearchOMDBCtrl.js
@@ -1,42 +1,16 @@
 "use strict";
 
-app.controller('SearchOMDBCtrl', function($scope, $location, $routeParams, temporaryFactory){
+app.controller('SearchOMDBCtrl', function($scope, $location, $routeParams, itemStorage){
   $scope.movieResults = [];
   $scope.searchMovie = "";
 
-
   $scope.searchForMovie = function() {
     console.log('searching for:', $scope.searchMovie);
-    temporaryFactory.searchOMDB($scope.searchMovie)
+    itemStorage.searchOMDB($scope.searchMovie)
       .then(function(searchResults){
         $scope.movieResults = searchResults;
         console.log($scope.movieResults)
-      })
-  }
-
-  // return $scope.searchResults
-
+      });
+  };
 });
 
-
-app.factory('temporaryFactory', function($q, $http){
-
-  
-  var searchOMDB = function(searchMovie) {
-    var items = []
-    return $q(function(resolve, reject){
-      $http.get(`http://www.omdbapi.com/?s=${searchMovie}&y=&plot=short&type=&r=json`)
-        .success(function(movieObject){
-          var searchResults = movieObject;
-            items.push(searchResults.Search)
-            resolve(items[0]);
-        }) //.success
-      }); //$q
-  };
-
-
-
-
-return {searchOMDB: searchOMDB}
-
-})
